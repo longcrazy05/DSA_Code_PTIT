5ed2116a5d540acd7095a3420dd10382987de0cb
longcrazy05 <longphungduc05@gmail.com>
2025-05-26 23:42:53 +0700

    Update Dijkstra_2.cpp

--- TRR2/Dijkstra_2.cpp
+++ TRR2/Dijkstra_2.cpp
@@ -2,51 +2,48 @@
 using namespace std;
 
 int n, m, s;
-vector<pair<int, int>> adj[1005];
 int d[1005];
 bool vs[1005];
+vector<pair<int,int>> adj[1005];
 
+void ktao(){
+	for(int i=1; i<=n; i++){
+		d[i]=1e9;
+		vs[i] = false;
+		adj[i].clear();
+	}
+}
 void dijkstra(int s) {
-    for(int i = 1; i <= n; i++) d[i] = 1e9;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
     d[s] = 0;
-    
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
     pq.push({0, s});
-    
-    while(!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-        
-        if(vs[u]) continue;
-        vs[u] = true;
-        
-        for(auto it : adj[u]) {
-            int v = it.first, w = it.second;
-            if(d[v] > d[u] + w) {
-                d[v] = d[u] + w;
-                pq.push({d[v], v});
-            }
-        }
-    }
-
-    for(int i = 1; i <= n; i++) cout << d[i] << " ";
-    cout << "\n";
+    while(pq.size()){
+    	int u = pq.top().second; pq.pop();
+    	if(vs[u]) continue;
+    	vs[u] = true;
+    	for(auto it: adj[u]){
+    		int v= it.first, w= it.second;
+    		if(d[v] > d[u] + w){
+    			d[v] = d[u] + w;
+    			pq.push({d[v], v});
+			}
+		}
+	}
 }
 
 int main() {
     int t; cin >> t;
     while(t--) {
         cin >> n >> m >> s;
-        for(int i = 1; i <= n; i++) adj[i].clear();
-        memset(vs, false, sizeof(vs));
-        
+        ktao();
         int x, y, w;
         while(m--) {
             cin >> x >> y >> w;
             adj[x].push_back({y, w});
             adj[y].push_back({x, w});
         }
-
         dijkstra(s);
+        for(int i=1; i<=n; i++) cout<<d[i]<<" ";
+        cout<<endl;
     }
 }
